7c308dd8b314a99c2a072a58af81b5b59a6ab752
tkuriyama <taro.kuriyama@gmail.com>
2020-11-12 11:15:52 -0500

    use Spacemonkey make

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 -- import Modules.HelloServer as HS
 -- import Modules.HelloServerAcid as HSA
-import Modules.HelloServerPersist as HSP
+-- import Modules.HelloServerPersist as HSP
+import Modules.Spacemonkey as SP
 
-main = HSP.main
+main = SP.main
